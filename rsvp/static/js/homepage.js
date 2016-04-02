771f08f7c8ab9c90b286daa75c3b9e417bcce730
Sherry Zhou <5643f56c@opayq.com>
2016-04-02 14:53:46 -0700

    Make wanderable page load onto our website

---
+++ rsvp/static/js/homepage.js
@@ -0,0 +1,47 @@
+$(function (argument) {
+
+  fillOutHoneyFund();
+
+  function fillOutHoneyFund () {
+    var myKey = "szarWanderablePage",
+        previousPageLoad = localStorage[myKey],
+        externalUrl = "wanderable.com/hm/szar";
+
+    if (previousPageLoad) {
+      previousPageLoad = JSON.parse(previousPageLoad);
+      loadData(previousPageLoad.data);
+      return;
+    }
+
+    $(document).ready(loadWanderable);
+
+     function loadWanderable() {
+       var QueryURL = "http://anyorigin.com/get?url=" + externalUrl + "&callback=?";
+        $.getJSON(QueryURL, function(data) {
+          if (data && data !== null && typeof data === "object" &&
+              data.contents && data.contents !== null &&
+              typeof data.contents === "string") {
+               data = data.contents.replace(/<script[^>]*>.*?<\/script>/g, "");
+               localStorage[myKey] = JSON.stringify({
+                 data: data,
+                 date: new Date(),
+               });
+               loadData(data);
+          }
+         });
+      }
+
+      function loadData (data) {
+        var $result = $(data).filter("div"),
+            $container = $('.queryResultContainer');
+        if (data.length > 0) {
+           $container.append($result.find("#hm-adonia"));
+           $container.append($result.find(".hm-item-layout"));
+           $container.find("a").each(function (i, el) {
+             el.href = "https://wanderable.com" + el.getAttribute("href");
+             el.target = "_blank";
+           });
+        }
+      }
+  }
+});
