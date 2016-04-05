279af1cacb28313985805e71f6ba4edc14d31fa2
Sherry Zhou <5643f56c@opayq.com>
2016-04-05 05:18:11 -0700

    Just use script template to get honeyfund working again.
    - Blah

--- rsvp/static/js/homepage.js
+++ rsvp/static/js/homepage.js
@@ -3,31 +3,13 @@ $(function (argument) {
   fillOutHoneyFund();
 
   function fillOutHoneyFund () {
-    var myKey = "szarWanderablePage",
-        previousPageLoad = localStorage[myKey],
-        externalUrl = "wanderable.com/hm/szar";
-
-    if (previousPageLoad) {
-      previousPageLoad = JSON.parse(previousPageLoad);
-      loadData(previousPageLoad.data);
-      return;
-    }
-
-    $(document).ready(loadWanderable);
-
-     loadData(rsvp.wanderable);
-
-      function loadData (data) {
-        var $result = $(data).filter("div"),
-            $container = $('.queryResultContainer .one-vh');
-        if (data.length > 0) {
-           $container.append($result.find("#hm-adonia .rl-location"));
-           $container.append($result.find(".hm-item-layout"));
-           $container.find("a").each(function (i, el) {
-             el.href = "https://wanderable.com" + el.getAttribute("href");
-             el.target = "_blank";
-           });
-        }
-      }
+    var $result = $($("#wanderable-text").html()),
+        $container = $('.queryResultContainer .one-vh');
+    $container.append($result.find("#hm-adonia .rl-location"));
+    $container.append($result.find(".hm-item-layout"));
+    $container.find("a").each(function (i, el) {
+      el.href = "https://wanderable.com" + el.getAttribute("href");
+      el.target = "_blank";
+    });
   }
 });
--- szar_site/views.py
+++ szar_site/views.py
@@ -1,7 +1,5 @@
 """RSVP Views"""
 from django.shortcuts import render
-from django.core.files import File
-import os
 
 import keen
 
@@ -9,8 +7,4 @@ from rsvp.views import KEEN_OBJECT
 
 def home(request):
     keen.add_event("visit_home_page", KEEN_OBJECT)
-    with open(os.path.dirname(__file__) + '/wanderable.txt', 'r') as myfile:
-        data = "".join(line.rstrip() for line in myfile)
-    return render(request, 'site/home.html', {
-        "wanderable": data
-    })
+    return render(request, 'site/home.html', {})
