e627316864bc94b18aad95efd5960f13efec560b
Sherry Zhou <5643f56c@opayq.com>
2016-04-05 19:39:50 -0700

    Improve carousel
        - Make swipe work on mobile (hopefully)
        - Set height of inner container in carousel to prevent navigation from moving up and down due to different image sizes

--- rsvp/static/js/homepage.js
+++ rsvp/static/js/homepage.js
@@ -1,6 +1,7 @@
 $(function (argument) {
 
   fillOutHoneyFund();
+  setCarousel();
 
   function fillOutHoneyFund () {
     var $result = $($("#wanderable-text").html()),
@@ -11,5 +12,42 @@ $(function (argument) {
       el.href = "https://wanderable.com" + el.getAttribute("href");
       el.target = "_blank";
     });
+  }
+
+  function setCarousel() {
+    var $carousel = $("#wedding-photos"),
+        $images = $carousel.find(".item img:not(.hidden)"), shortest,
+        $carouselInner = $carousel.find(".carousel-inner"),
+        $rightArrow = $carousel.find(".right.carousel-control"),
+        $leftArrow = $carousel.find(".left.carousel-control");
+    // Set the minimum height as height of carousel, prevents moving up and down
+    $images.imagesLoaded(function (il) {
+      il.elements.forEach(function (el, i) {
+        var $parent = $(el).parent(),
+            oldActive = $parent.hasClass("active");
+        $parent.addClass("active");
+        if (shortest) {
+          shortest = Math.min(shortest, el.clientHeight);
+        } else {
+          shortest = el.clientHeight;
+        }
+        if (!oldActive) {
+          $parent.removeClass("active");
+        }
+      });
+      $carouselInner.css("height", shortest);
+    });
+
+    if (Modernizr.touch) {
+      $carousel.on("swiperight.changeCarousel", function (e) {
+        $leftArrow.trigger("click");
+        $leftArrow.trigger("click");
+
+      });
+      $carousel.on("swipeleft.changeCarousel", function (e) {
+        $rightArrow.trigger("click");
+        $rightArrow.trigger("click");
+      });
+    }
   }
 });
