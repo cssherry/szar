e2f87e5e8cb2dec81b169fe8fe62095162371a67
Sherry Zhou <5643f56c@opayq.com>
2016-04-11 15:07:55 -0700

    Updates page's hash upon scroll
    - Not going to include because it's a bit buggy, slows down page scroll
    and when switching from "photos" to "about" section slowly, there's
    strange navbar jump down and then back up.

--- rsvp/static/js/homepage.js
+++ rsvp/static/js/homepage.js
@@ -2,6 +2,7 @@ $(function (argument) {
 
   fillOutHoneyFund();
   setCarousel();
+  setScrollEvent();
 
   function fillOutHoneyFund () {
     var $result = $($("#wanderable-text").html()),
@@ -57,6 +58,47 @@ $(function (argument) {
         }
       });
       $carouselInner.css("height", shortest);
+    }
+  }
+  var hashValue;
+  function setScrollEvent () {
+    $(document).on('scroll',debounce(function () {
+      //begins before top but ends in visible area
+      //+ 10 allows you to change hash before it hits the top border
+      $('.wedding-section').each(function(){
+          if ($(this).offset().top < window.pageYOffset + 15 &&
+              $(this).offset().top + $(this).height() > window.pageYOffset + 15) {
+                hashValue = "#" + $(this).attr('id');
+                if (location.hash !== hashValue) {
+                  // http://stackoverflow.com/questions/3870057/how-can-i-update-window-location-hash-without-jumping-the-document
+                  if(history.pushState) {
+                      history.pushState(null, null, hashValue);
+                  }
+                  else {
+                      location.hash = $(this).attr('id');
+                  }
+                }
+          }
+      });
+    }, 200));
+    // From https://davidwalsh.name/javascript-debounce-function
+    // Returns a function, that, as long as it continues to be invoked, will not
+    // be triggered. The function will be called after it stops being called for
+    // N milliseconds. If `immediate` is passed, trigger the function on the
+    // leading edge, instead of the trailing.
+    function debounce(func, wait, immediate) {
+    	var timeout;
+    	return function() {
+    		var context = this, args = arguments;
+    		var later = function() {
+    			timeout = null;
+    			if (!immediate) func.apply(context, args);
+    		};
+    		var callNow = immediate && !timeout;
+    		clearTimeout(timeout);
+    		timeout = setTimeout(later, wait);
+    		if (callNow) func.apply(context, args);
+    	};
     }
   }
 });
