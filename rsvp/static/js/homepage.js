a098780bd5ab299d652e11dc29764a3aa803b719
Sherry Zhou <5643f56c@opayq.com>
2016-04-13 12:32:08 -0700

    Make photos in carousel narrower than 100% width
    - Apparently people don't like the full width :(

--- rsvp/static/js/homepage.js
+++ rsvp/static/js/homepage.js
@@ -21,13 +21,6 @@ $(function (argument) {
         $carouselInner = $carousel.find(".carousel-inner"),
         $rightArrow = $carousel.find(".right.carousel-control"),
         $leftArrow = $carousel.find(".left.carousel-control");
-    // Set the minimum height as height of carousel, prevents moving up and down
-    $images.imagesLoaded(function (il) {
-      resizeCarousel($images);
-    });
-    $(window).resize(function () {
-      resizeCarousel($images);
-    });
 
     if (Modernizr.touchevents) {
       $swipearea.on("swiperight.changeCarousel", function (e) {
@@ -38,25 +31,6 @@ $(function (argument) {
         $rightArrow.trigger("click");
         $rightArrow.trigger("click");
       });
-    }
-
-    function resizeCarousel (els) {
-      var shortest;
-      $carouselInner.css('height', "auto");
-      els.each(function (i, el) {
-        var $parent = $(el).parent(),
-            oldActive = $parent.hasClass("active");
-        $parent.addClass("active");
-        if (shortest) {
-          shortest = Math.min(shortest, el.clientHeight);
-        } else {
-          shortest = el.clientHeight;
-        }
-        if (!oldActive) {
-          $parent.removeClass("active");
-        }
-      });
-      $carouselInner.css("height", shortest);
     }
   }
 });
