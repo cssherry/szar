c8a5642084d2ab99f87b654c2de41371008d9594
Sherry Zhou <5643f56c@opayq.com>
2016-04-12 21:02:26 -0700

    Collapse navbar upon click

--- rsvp/static/js/base.js
+++ rsvp/static/js/base.js
@@ -12,6 +12,10 @@ $(function () {
     $heart.css("height", 110);
     $letters.removeClass("hidden");
   });
+  $navbar = $('.navbar-collapse');
+  $navbar.on('click.collapseNavbar', 'li', function(e) {
+    $navbar.collapse('hide');
+  });
   // Upgrade images, from http://stackoverflow.com/a/31370466/4607533
   imagesLoaded($("img.hidden")).on("progress", upgradeImage);
   function upgradeImage(_imgloaded, image, el) {
