40ecd7c78f2bbe6cffd8395925f80a0b26685348
Sherry Zhou <5643f56c@opayq.com>
2016-04-13 20:55:23 -0700

    Add Google Webmaster Tools verification

--- szar_site/urls.py
+++ szar_site/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     # http://stackoverflow.com/questions/20480177/what-more-do-i-need-to-do-to-have-djangos-login-required-decorator-work#comment37075291_20480226
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'},name="my_login"),
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    url(r'^google7a6f5afec5a29ae0.html$', TemplateView.as_view(template_name='ext_google7a6f5afec5a29ae0.html')),
     url(r'^favicon\.ico$', RedirectView.as_view(url=settings.STATIC_URL + 'images/favicon.ico')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home, name='root-url'),
