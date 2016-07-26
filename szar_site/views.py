01f7b9f83b2bb5865dcbf29c78145b256e85086a
Sherry Zhou <5643f56c@opayq.com>
2016-07-26 00:08:50 -0700

    Prevent non-users from accessing attendee list

--- szar_site/views.py
+++ szar_site/views.py
@@ -1,6 +1,7 @@
 """RSVP Views"""
 from django.shortcuts import render
 from rsvp.view_helpers import rsvps_get_attending_raw
+from django.contrib.auth.models import User
 
 import keen
 
@@ -9,7 +10,7 @@ from rsvp.view_helpers import KEEN_OBJECT
 def home(request, username=None):
     keen.add_event("visit_home_page", KEEN_OBJECT)
     context = {}
-    if username:
+    if User.objects.filter(username=username).exists():
         context["attendees"] = rsvps_get_attending_raw()
         context["url_username"] = username
     else:
