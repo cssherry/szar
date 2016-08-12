ac8c704b56738f7b4b24d24009448b0c0be076cb
Sherry Zhou <5643f56c@opayq.com>
2016-08-11 22:14:42 -0700

    Try no path info

--- szar_site/middleware/url_redirect_middleware.py
+++ szar_site/middleware/url_redirect_middleware.py
@@ -10,4 +10,4 @@ class UrlRedirectMiddleware:
     """
     def process_request(self, request):
         if 'szarwed.herokuapp.com' == request.META['HTTP_HOST']:
-            return HttpResponsePermanentRedirect('https://www.szar.us' + request.META['PATH_INFO'])
+            return HttpResponsePermanentRedirect('https://www.szar.us')
