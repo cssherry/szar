06c2becd6530a7065a99cea3a9f1a234dd18de7f
Sherry Zhou <5643f56c@opayq.com>
2016-09-02 21:58:37 -0700

    Add gift info to model

--- rsvp/models.py
+++ rsvp/models.py
@@ -15,6 +15,28 @@ class RSVP(models.Model):
         (SATURDAY, "Saturday evening reception"),
         (SUNDAY, "Sunday morning wedding ceremony and brunch"),
     )
+    AIRFARE = 0
+    DAY1 = 1
+    DAY2 = 2
+    DAY3 = 3
+    DAY4 = 4
+    DAY5 = 5
+    DAY6 = 6
+    DAY7 = 7
+    DAY8 = 8
+    OTHER = -1
+    GIFT_TYPE = (
+      (AIRFARE, "Airfare"),
+      (DAY1, "Day 1 in Bhutan"),
+      (DAY2, "Day 2 in Bhutan"),
+      (DAY3, "Day 3 in Bhutan"),
+      (DAY4, "Day 4 in Bhutan"),
+      (DAY5, "Day 5 in Bhutan"),
+      (DAY6, "Day 6 in Bhutan"),
+      (DAY7, "Day 7 in Bhutan"),
+      (DAY8, "Day 8 in Bhutan"),
+      (OTHER, "Other"),
+    )
     attending_dates = models.CommaSeparatedIntegerField(max_length=4,
                                                         choices=DATE_OPTIONS,
                                                         blank=True)
@@ -24,7 +46,7 @@ class RSVP(models.Model):
     plus_one = models.BooleanField(default=False)
     plus_one_name = models.CharField(max_length=200, null=True, unique=True, default=None)
     song_requests = models.TextField(default="", blank=True)
-    gift = models.CharField(max_length=200, blank=True)
+    gift = models.NullBooleanField(default=False)
     formal_prefix = models.CharField(max_length=200, blank=True)
     affiliation = models.CharField(default="", max_length=200)
     number_attendees = models.IntegerField(default=None, null=True, blank=True)
@@ -33,6 +55,14 @@ class RSVP(models.Model):
     edited_date = models.DateTimeField(default=timezone.now, blank=True)
     sent_emails = models.TextField(default="{}", blank=True)
     comments = models.TextField(default="", blank=True)
+    gift_type = models.CommaSeparatedIntegerField(max_length=1,
+                                                  choices=GIFT_TYPE,
+                                                  null=True,
+                                                  blank=True)
+    gift_amount = models.DecimalField(default=0, max_digits=4, decimal_places=2)
+    gift_message = models.TextField(default="", blank=True)
+    thank_you_message = models.TextField(default="", blank=True)
+    address = models.TextField(default="", blank=True)
 
     def edit(self, new_values):
         """Publishes RSVP"""
